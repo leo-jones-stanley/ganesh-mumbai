1ed4d21ab0d4e7c5f4de3e49b9307b59bc8b987b
leo jones <stanleyleojones@gmail.com>
2021-12-23 00:39:08 +0530

    Update cardview.comp.jsx

--- m3a-web/src/components/login-page/cardview.comp.jsx
+++ m3a-web/src/components/login-page/cardview.comp.jsx
@@ -2,17 +2,18 @@ import React from "react";
 import './card.style.css';
 import Form_view from './form/form.comp'
 import Logo_view from './logo/logo.component'
+import Card from '@material-ui/core'
 // import { Card } from '@mui/material';
 
 
 const Card_view = (props) => {
     return (
         <div>
-        
+        <card className="card">
          <h1>card view</h1>   
          <Logo_view />
          <Form_view />
-
+         </card>
         </div>
     )
 }
