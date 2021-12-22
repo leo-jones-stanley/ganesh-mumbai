33cc33403efb8449d598d502d2480ff8ff784b9b
leo jones <stanleyleojones@gmail.com>
2021-12-22 10:50:31 +0530

    Create login.js

---
+++ m3a-web/src/login.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Card_view from './components/login-page/cardview.comp'
+import './login.css'
+
+const Login = (props) =>{
+    return(
+        <div>
+            <h1>jones</h1>
+             <Card_view />
+        </div>
+    )
+}
+export default Login
